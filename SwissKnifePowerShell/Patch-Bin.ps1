7d187c2b068ef2ae5ebae3d65c7b69dfb93a434c
Jose Ramón Lambea <spageek@users.noreply.github.com>
2014-06-11 09:03:14 +0200

    Update Patch-Bin.ps1

--- SwissKnifePowerShell/Patch-Bin.ps1
+++ SwissKnifePowerShell/Patch-Bin.ps1
@@ -16,6 +16,35 @@
 
 #>
 
+<#
+.SYNOPSIS
+Apply patches as binary.
+
+.DESCRIPTION
+This is a template.
+
+.PARAMETER File 
+File to patch.
+
+.PARAMETER Offset
+The byte address to start write data, the format of this Offset could be decimal (3735928559d), hexadecimal (0xdeadbeef) or binary (11011110101011011011111011101111b).
+
+.PARAMETER Bytes_to_Write
+The data to write, the format could be as array of bytes value in decimal (235d), hexadecimal (0xEB) or binary (11101011b).
+
+.EXAMPLE
+Write a JMP (Hex) in a example.exe file at address 0x452EF3.
+
+Patch-Bin.ps1 -File example.exe -Offset 0x452EF3 -Bytes_to_Write 0xEB
+
+.EXAMPLE 
+Write a "A" char in a example.txt file at address byte number 10.
+
+Patch-Bin.ps1 -File example.txt -Offset 10d -Bytes_to_Write 65d
+
+.NOTES
+
+#>
 
 Param(
     [parameter(Mandatory=$true)]
